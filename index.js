54f13525fd31018cd8ea5059c5f9ca35fe7e8124
dhershman <dhershman@dealertire.com>
2018-01-30 12:30:25 -0500

    replace hapi with vanilla http setup

--- index.js
+++ index.js
@@ -1,60 +1,26 @@
-// const hapi = require('hapi');
-const axios = require('axios');
-// const server = new hapi.Server();
 const http = require('http');
+const axios = require('axios');
 
-// server.connection({
-// 	port: process.env.PORT || 5000
-// });
-
-// server.route({
-// 	path: '/{path?}',
-// 	method: 'POST',
-// 	handler: (request, reply) => {
-// 		axios.get('http://inspirobot.me/api?generate=true').then(({data}) => {
-// 			const slackReply = {
-// 				response_type: "in_channel",
-// 				text: data
-// 			};
-
-// 			reply(slackReply);
-// 		});
-// 	}
-// });
-
-// server.start((err) => {
-// 	if (err) {
-// 		throw err;
-// 	}
-
-// 	console.log('Server running at: ' + server.info.uri);
-// 	server.log('info', 'Server running at: ' + server.info.uri);
-// });
-
+const port = process.env.PORT || 5000;
 
 http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	if (req.url === '/') {
-		res.write(req.url);
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	axios.get('http://inspirobot.me/api?generate=true').then(({ data }) => {
+		const slackReply = {
+			response_type: "in_channel",
+			text: data
+		};
+
+		res.write(JSON.stringify(slackReply));
 		res.end();
-	} else {
-		axios.get('http://inspirobot.me/api?generate=true').then(({ data }) => {
-			const slackReply = {
-				response_type: "in_channel",
-				text: data
-			};
+	}).catch(err => {
+		const slackReply = {
+			text: 'http://inspirobot.me/website/images/inspirobot-dark-green.png'
+		};
 
-			res.write(slackReply);
-			res.end();
-		}).catch(err => {
-			const slackReply = {
-				text: 'http://inspirobot.me/website/images/inspirobot-dark-green.png'
-			};
+		console.log(err);
 
-			console.log(err);
-
-			res.write(slackReply);
-			res.end();
-		});
-	}
-}).listen(8080);
+		res.write(JSON.stringify(slackReply));
+		res.end();
+	});
+}).listen(port);
